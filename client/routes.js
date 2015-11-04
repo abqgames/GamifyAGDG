c1631e89c2e50a1597fc014f8baa38c52c549249
vynlar <lightningzygella@gmail.com>
2015-11-03 21:57:49 -0700

    added a userProfile template and route, added login buttons, added baseLayout file

--- client/routes.js
+++ client/routes.js
@@ -1,6 +1,36 @@
+/*
+ * Configuration
+ */
+Router.configure({
+  layoutTemplate: "baseLayout",
+  notFoundTemplate: "baseLayout",
+  loadingTemplate: "baseLayout"
+});
+
+Accounts.ui.config({
+  passwordSignupFields: "USERNAME_ONLY"
+});
+
+/*
+ * Routes
+ */
 Router.route("/", function() {
   this.render("home");
 });
-Router.route("/attend", function() {
+Router.route("/signin", function() {
   this.render("attendence");
+});
+Router.route("/user/:username", function() {
+  var user = Meteor.users.findOne({username: this.params.username});
+  if(!user) {
+    this.render("404");
+  } else {
+    this.render("userProfile", {
+      data: function() {
+        return {
+          user: user
+        }
+      }
+    });
+  }
 });
