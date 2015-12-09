3b3224374da6a552203a05775114ab2d5d5edba1
vynlar <lightningzygella@gmail.com>
2015-12-08 20:51:50 -0700

    fixed signin issue

---
+++ client/lib/init.js
@@ -0,0 +1,1 @@
+Session.set("username",Meteor.user().username);
--- client/templates/attendance/attendance.js
+++ client/templates/attendance/attendance.js
@@ -1,6 +1,6 @@
 Template.attendance.events({
   "click #attendance-button": function(e) {
-    Meteor.call("user.attend", function(err, res) {
+    Meteor.call("user.attend", {username: Session.get("username")}, function(err, res) {
       if(err)
         console.log(err);
       else
