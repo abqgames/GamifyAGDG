883995be5de212b90f42147609caa3ae1191ccc0
vynlar <lightningzygella@gmail.com>
2016-03-01 19:09:14 -0700

    sign-in form

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -1,5 +1,6 @@
 Handlebars.registerHelper("isAdmin", function() {
   var user = Meteor.user();
+  if(user == undefined) return false;
   if(user.profile == undefined) return false;
   if(user.profile.isAdmin) return true;
   return false;
